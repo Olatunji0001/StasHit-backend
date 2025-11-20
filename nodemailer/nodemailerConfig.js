ac4bb8ac30e5b8cc28d2d51378b2f8aa51deeab7
olatunji0001 <musbautunji@gmail>
2025-11-20 22:00:48 +0100

    new update

--- nodemailer/nodemailerConfig.js
+++ nodemailer/nodemailerConfig.js
@@ -6,7 +6,9 @@ dotenv.config();
 const password = process.env.PASSWORD;
 const gmail = process.env.GMAIL;
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com", // explicitly set Gmail SMTP
+  port: 465, // SSL
+  secure: true,
   auth: {
     user: gmail,
     pass: password,
